1798842d85590ad6a9f52669e456dfb7b2c8f160
Franz Łotocki <flotocki002@gmail.com>
2021-06-12 23:35:55 +0200

    a a a

---
+++ test.c
@@ -0,0 +1,23 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  test.c
+ *
+ *    Description:  test pkg 
+ *
+ *        Version:  1.0
+ *        Created:  06/12/2021 11:30:31 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+int main() {
+    printf("test\n");
+    return(0);
+}
